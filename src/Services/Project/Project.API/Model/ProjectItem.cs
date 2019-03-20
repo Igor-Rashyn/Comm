26feaaca7b8fd2ec869e3cd26447798fa28b8d17
Nish <igor.rashyn@gmail.com>
2019-03-21 10:44:55 +1300

    first commit

---
+++ src/Services/Project/Project.API/Model/ProjectItem.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Project.API.Model
+{
+    public class ProjectItem
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        //public List<File> Files { get; set; }
+        //public List<City> Cities { get; set; }
+        //public List<Country> Countries { get; set; }
+        //public List<ProjectCategory> Categories { get; set; }
+        //public ProjectStatus Status { get; set; }
+        //public ProjectType Type { get; set; }
+        //public List<Task> Tasks { get; set; }
+        //public List<Chat> Chats { get; set; }
+        //public List<User> Subscribers { get; set; }
+        //public List<User> Team { get; set; }
+
+    }
+}
