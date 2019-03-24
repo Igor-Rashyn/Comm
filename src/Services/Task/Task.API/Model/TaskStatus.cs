360c593d2d42b4778d54c15213d60552464d7deb
Nish <igor.rashyn@gmail.com>
2019-03-25 12:03:28 +1300

    init Task.API

---
+++ src/Services/Task/Task.API/Model/TaskStatus.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Task.API.Model
+{
+    public class TaskStatus
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
