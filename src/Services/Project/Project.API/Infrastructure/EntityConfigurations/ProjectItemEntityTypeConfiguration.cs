8513554226aba39cc040d038074af972066efb2b
Nish <igor.rashyn@gmail.com>
2019-03-25 12:07:16 +1300

    added Project context

---
+++ src/Services/Project/Project.API/Infrastructure/EntityConfigurations/ProjectItemEntityTypeConfiguration.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Project.API.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Project.API.Infrastructure.EntityConfigurations
+{
+    public class ProjectItemEntityTypeConfiguration : IEntityTypeConfiguration<ProjectItem>
+    {
+        public void Configure(EntityTypeBuilder<ProjectItem> builder)
+        {
+            builder.ToTable("Project");
+
+            builder.Property(p => p.Id)
+                .ForSqlServerUseSequenceHiLo("project_hilo")
+                .IsRequired();
+
+            builder.Property(p => p.Created)
+                .HasDefaultValueSql("CONVER(date, GETDATE())");
+
+            builder.Property(p => p.Updated)
+                .ValueGeneratedOnAddOrUpdate();
+
+            builder.Property(p => p.Updated)
+                .Metadata.AfterSaveBehavior = Microsoft.EntityFrameworkCore.Metadata.PropertySaveBehavior.Ignore;
+
+            builder.Property(p => p.Title)
+                .HasMaxLength(60);
+
+            builder.Property(p => p.Subtitle)
+                .HasMaxLength(135);
+
+            builder.HasOne(p => p.ProjectStatus)
+                .WithMany()
+                .HasForeignKey(p => p.ProjectStatusId);
+
+            builder.HasOne(p => p.ProjectType)
+                .WithMany()
+                .HasForeignKey(p => p.ProjectTypeId);
+
+            builder.HasOne(p => p.ProjectCategory)
+                .WithMany()
+                .HasForeignKey(p => p.ProjectCategoryId);
+        }
+
+    }
+}
