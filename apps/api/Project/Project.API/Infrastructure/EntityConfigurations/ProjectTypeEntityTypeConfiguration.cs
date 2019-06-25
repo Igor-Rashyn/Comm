6a5a4f2b162f6f61a9bab051335209242c03c332
Igor Rashyn <igor.rashyn@youdo.co.nz>
2019-06-26 08:41:26 +1200

    init Project service

---
+++ apps/api/Project/Project.API/Infrastructure/EntityConfigurations/ProjectTypeEntityTypeConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Project.API.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Project.API.Infrastructure.EntityConfigurations
+{
+    public class ProjectTypeEntityTypeConfiguration : IEntityTypeConfiguration<ProjectType>
+    {
+        public void Configure(EntityTypeBuilder<ProjectType> builder)
+        {
+            builder.ToTable("ProjectType");
+
+            builder.HasKey(ps => ps.Id);
+
+            builder.Property(ps => ps.Id)
+                .ForSqlServerUseSequenceHiLo("project_type_hilo")
+                .IsRequired();
+
+            builder.Property(ps => ps.Name)
+                .IsRequired()
+                .HasMaxLength(50);         
+        }
+
+    }
+}
