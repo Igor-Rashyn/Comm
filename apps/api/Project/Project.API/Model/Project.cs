2432ca741a393c96a1badcd0036d2fb91deda0ed
Igor Rashyn <igor.rashyn@youdo.co.nz>
2019-07-01 10:01:57 +1200

    add TODO: for project

--- apps/api/Project/Project.API/Model/Project.cs
+++ apps/api/Project/Project.API/Model/Project.cs
@@ -5,49 +5,56 @@ using System.Threading.Tasks;
 
 namespace Project.API.Model
 {
-    public class Project
+  public class Project
+  {
+    public Guid Id { get; private set; }
+    public string OwnerId { get; private set; }
+    public string Title { get; set; }
+    public int ProjectCategoryId { get; set; }
+    public ProjectCategory ProjectCategory { get; set; }
+    public int ProjectStatusId { get; set; }
+    public ProjectStatus ProjectStatus { get; set; }
+    public int ProjectTypeId { get; set; }
+    public ProjectType ProjectType { get; set; }
+    public string Subtitle { get; set; }
+    public string Description { get; set; } // нужно поле с возможность писать текст/ вставлять картинки и т.д как ворлд react-contenteditable https://github.com/jpuri/react-draft-wysiwyg rich text editor 
+    public DateTime? Duration { get; set; }
+    public DateTime Created { get; set; }
+    public DateTime Updated { get; set; }
+    //public Comment Comments { get; set; } // комментарии как ворлд документ, можно вставлять линки видео картинки и дизайнить текст
+    //public string GoogleAnalytics { get; set; }
+    //public string ProjectImage { get; set; }
+    //public string ProjectVideo {get;set;}
+    //public List<File> Files { get; set; }
+    //public List<City> Cities { get; set; }
+    //public List<Country> Countries { get; set; }
+    private List<ProjectTask> _Tasks { get; set; }
+    public IEnumerable<ProjectTask> Tasks => _Tasks.AsReadOnly();
+    //public List<Chat> Chats { get; set; }
+    //public List<User> Subscribers { get; set; }
+    //public List<User> Team { get; set; }
+
+    protected Project()
     {
-        public int Id { get; private set; }
-        public string OwnerId { get; private set; }
-        public string Title { get; set; }
-        public int ProjectCategoryId { get; set; }
-        public ProjectCategory ProjectCategory { get; set; }
-        public int ProjectStatusId { get; set; }
-        public ProjectStatus ProjectStatus { get; set; }
-        public int ProjectTypeId { get; set; }
-        public ProjectType ProjectType { get; set; }
-        public string Subtitle { get; set; }
-        public string Description { get; set; } // нужно поле с возможность писать текст/ вставлять картинки и т.д как ворлд react-contenteditable https://github.com/jpuri/react-draft-wysiwyg rich text editor 
-        public DateTime? Duration { get; set; }
-        public DateTime Created { get; set; }
-        public DateTime Updated { get; set; }
-        //public Comment Comments { get; set; } // комментарии как ворлд документ, можно вставлять линки видео картинки и дизайнить текст
-        //public string GoogleAnalytics { get; set; }
-        //public string ProjectImage { get; set; }
-        //public string ProjectVideo {get;set;}
-        //public List<File> Files { get; set; }
-        //public List<City> Cities { get; set; }
-        //public List<Country> Countries { get; set; }
-        private List<ProjectTask> _Tasks { get; set; }
-        public IEnumerable<ProjectTask> Tasks => _Tasks.AsReadOnly();
-        //public List<Chat> Chats { get; set; }
-        //public List<User> Subscribers { get; set; }
-        //public List<User> Team { get; set; }
-
-        protected Project()
-        {
-            _Tasks = new List<ProjectTask>();
-        }
-
-        public Project(string userId) {
-            OwnerId = !string.IsNullOrWhiteSpace(userId) ? userId : throw new ArgumentNullException(nameof(userId));
-        }
-
-        public ProjectTask AddTask() {
-
-        }
+      _Tasks = new List<ProjectTask>();
+      this.Id = Guid.NewGuid();
     }
+
+    public Project(string userId)
+    {
+      OwnerId = !string.IsNullOrWhiteSpace(userId) ? userId : throw new ArgumentNullException(nameof(userId));
+    }
+
+    public ProjectTask AddTask()
+    {
+
+    }
+  }
 }
+
+//добавить город, страну
+//категорий может быть несколько
+//Участников может быть несколько (подумать, если участники member, есть подпищики)
 
 // дизвайн для веба
 // похожий на кикстартер
